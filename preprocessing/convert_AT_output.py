05afa0dfa39a9037538b58f6bf450360153bcc4f
jrawling <jrawling@cern.ch>
2018-10-17 10:54:42 +0100

    Added jet, W lepton and top truth information

--- preprocessing/convert_AT_output.py
+++ preprocessing/convert_AT_output.py
@@ -19,19 +19,59 @@ def main():
         variables_to_save={
             # Reco level
             'n_jets': 'Length$(jet_pt)',
-            # 'jet_pt_0': 'Jet.PT[0]',
-            # 'jet_eta_0': 'Jet.Eta[0]',
-            # 'jet_phi_0': 'Jet.Phi[0]',
-            # 'jet_m_0': 'Jet.Mass[0]',
+            'jet_pt_0': 'jet_pt[0]/1e3',
+            'jet_eta_0': 'jet_eta[0]',
+            'jet_phi_0': 'jet_phi[0]',
+            'jet_e_0': 'jet_e[0]/1e3',
+
+            'jet_isBjet_0': '(jet_nGhosts_bHadron[0] > 0)',
+            'jet_pt_1': 'jet_pt[1]/1e3',
+            'jet_eta_1': 'jet_eta[1]',
+            'jet_phi_1': 'jet_phi[1]',
+            'jet_e_1': 'jet_e[1]/1e3',
+
+            'jet_isBjet_1': '(jet_nGhosts_bHadron[1] > 0)',
+            'jet_pt_2': 'jet_pt[2]/1e3',
+            'jet_eta_2': 'jet_eta[2]',
+            'jet_phi_2': 'jet_phi[2]',
+            'jet_e_2': 'jet_e[2]/1e3',
+            'jet_isBjet_2': '(jet_nGhosts_bHadron[2] > 0)',
+
+            'jet_pt_3': 'jet_pt[3]/1e3',
+            'jet_eta_3': 'jet_eta[3]',
+            'jet_phi_3': 'jet_phi[3]',
+            'jet_e_3': 'jet_e[3]/1e3',
+            'jet_isBjet_3': '(jet_nGhosts_bHadron[3] > 0)',
+
+            'jet_pt_4': 'jet_pt[4]/1e3',
+            'jet_eta_4': 'jet_eta[4]',
+            'jet_phi_4': 'jet_phi[4]',
+            'jet_e_4': 'jet_e[4]/1e3',
+            'jet_isBjet_4': '(jet_nGhosts_bHadron[4] > 0)',
+
+            'W_lep_pt': 'jet_pt[4]/1e3',
+            'W_lep_eta': 'jet_eta[4]',
+            'W_lep_phi': 'jet_phi[4]',
+            'W_lep_m': 'jet_e[4]/1e3',
+
+            'met': 'met_met',
 
             # Ground truth
             # 'truth_had_top_pt': 'GenJet.PT[0]',
+            'truth_had_top_pt':  'truth.MC_tbar_afterFSR_pt/1e3',
             'truth_had_top_eta': 'truth.MC_tbar_afterFSR_eta',
+            'truth_had_top_phi': 'truth.MC_tbar_afterFSR_phi',
+            'truth_had_top_m':   'truth.MC_tbar_afterFSR_m/1e3',
+
+            'truth_lep_top_pt':  'truth.MC_tbar_afterFSR_pt/1e3',
+            'truth_lep_top_eta': 'truth.MC_tbar_afterFSR_eta',
+            'truth_lep_top_phi': 'truth.MC_tbar_afterFSR_phi',
+            'truth_lep_top_m':   'truth.MC_tbar_afterFSR_m/1e3',
             # 'truth_had_top_phi': 'GenJet.Phi[0]',
             # 'truth_had_top_m': 'GenJet.Mass[0]',
             # 'truth_n_jets': 'GenJet_size'
         },
-        selection='',
+        selection='1.0',
         tuple_name='particleLevel',
         friend_tuple_name='truth',
         major_index="(runNumber<<13)",
